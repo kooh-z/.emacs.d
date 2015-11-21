936628ea6fc68718e0551bb2a3cf521a2d372999
kooh-z <kody.j.gigs@gmail.com>
2015-11-22 00:36:12 +0900

    add all packages

---
+++ elpa/enh-ruby-mode-20150711.1530/enh-ruby-mode-autoloads.el
@@ -0,0 +1,31 @@
+;;; enh-ruby-mode-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+
+
+;;;### (autoloads (enh-ruby-mode) "enh-ruby-mode" "enh-ruby-mode.el"
+;;;;;;  (21981 56772 0 0))
+;;; Generated autoloads from enh-ruby-mode.el
+
+(autoload 'enh-ruby-mode "enh-ruby-mode" "\
+Enhanced Major mode for editing Ruby code.
+
+\\{enh-ruby-mode-map}
+
+\(fn)" t nil)
+
+;;;***
+
+;;;### (autoloads nil nil ("enh-ruby-mode-pkg.el") (21981 56772 500291
+;;;;;;  0))
+
+;;;***
+
+(provide 'enh-ruby-mode-autoloads)
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; coding: utf-8
+;; End:
+;;; enh-ruby-mode-autoloads.el ends here
