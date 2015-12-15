ba45c68ebcd9bb0cffe0670c6c3e9c963c505fdf
kooh-z <kody.j.gigs@gmail.com>
2015-12-16 00:49:00 +0900

    rails elpa

---
+++ elpa/rake-20150831.158/rake-autoloads.el
@@ -0,0 +1,43 @@
+;;; rake-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+
+
+;;;### (autoloads (rake rake-find-task rake-regenerate-cache rake-rerun)
+;;;;;;  "rake" "rake.el" (22128 13816 0 0))
+;;; Generated autoloads from rake.el
+
+(autoload 'rake-rerun "rake" "\
+Re-runs the last task
+
+\(fn)" t nil)
+
+(autoload 'rake-regenerate-cache "rake" "\
+Regenerates the rake's cache for the current project.
+
+\(fn)" t nil)
+
+(autoload 'rake-find-task "rake" "\
+Finds a rake task.
+
+\(fn ARG)" t nil)
+
+(autoload 'rake "rake" "\
+Runs rake command.
+
+\(fn ARG &optional COMPILATION-MODE)" t nil)
+
+;;;***
+
+;;;### (autoloads nil nil ("rake-pkg.el") (22128 13816 132993 0))
+
+;;;***
+
+(provide 'rake-autoloads)
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; coding: utf-8
+;; End:
+;;; rake-autoloads.el ends here
